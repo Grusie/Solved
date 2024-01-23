3c2a73f3a600be36c72fdf3834158f2d9ee8d965
Grusie <75468060+Grusie@users.noreply.github.com>
2024-01-23 11:16:26 +0900

    [Silver III] Title: 주유소, Time: 356 ms, Memory: 40376 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.kt
@@ -0,0 +1,68 @@
+package baekjoon
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+/**
+ * 아이디어 : 처음 주유소의 가격과 비교했을 때, 그것보다 낮은 가격의 주유소가 나올 때 까지의 거리만큼만 기름을 넣고
+ *          그 이후, 도착 했을 때에도 마찬가지로 비교하여 기름을 넣으면 된다.
+ *
+ * 시간 복잡도 : N!?
+ *
+ * 변수 : 각 거리를 담은 배열, 유류비를 담은 배열, 총 유류비
+ **/
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val order = br.readLine().toInt()
+    val city = IntArray(order)
+    val dist = IntArray(order)
+
+    with(StringTokenizer(br.readLine())){
+        repeat(order - 1){
+            dist[it + 1] = nextToken().toInt()
+        }
+    }
+
+    with(StringTokenizer(br.readLine())){
+        repeat(order){
+            city[it] = nextToken().toInt()
+        }
+    }
+
+    var totalCost = 0
+    var totalDist = 0
+    var index = 0
+    while(index < city.size){
+
+        var flag = true
+        var tempDist = 0
+        for(i in index + 1 until city.size){
+            if(city[index] > city[i]){
+                flag = false
+                for(j in index + 1 .. i) {
+                    tempDist += dist[j]
+                }
+
+                totalCost += tempDist * city[index]
+                totalDist += tempDist
+                index = i
+                break
+            }
+        }
+
+        if(flag){
+            totalCost += (dist.sum() - totalDist) * city[index]
+            break
+        }
+    }
+
+    bw.write("$totalCost")
+
+    bw.flush()
+    bw.close()
+}
